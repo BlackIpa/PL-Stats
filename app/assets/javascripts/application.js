b62174617c091f30cbbfd00a0336e57deca98f18
Piotr Augustyniak <augustyniak.p@gmail.com>
2017-10-16 19:51:25 +0200

    Refactoring javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,98 +17,85 @@
 //= require bootstrap-sprockets
 //= require_tree .
 
-// TEAMS INDEX VIEW
-
 $(document).ready( function(){
   $("#t_over").click(function(){
-    $('.team_table').addClass("collapse");
-    $('#over_t').removeClass("collapse");
-    $('.t_sts_btn').removeClass("sts_active");
-    $('#t_over').addClass("sts_active");
+    addAndRemoveClass('.team_table', '#over_t', '.t_sts_btn', '#t_over')
   });
   $("#t_offe").click(function(){
-    $('.team_table').addClass("collapse");
-    $('#offe_t').removeClass("collapse");
-    $('.t_sts_btn').removeClass("sts_active");
-    $('#t_offe').addClass("sts_active");
+    addAndRemoveClass('.team_table', '#offe_t', '.t_sts_btn', '#t_offe')
   });
   $("#t_deff").click(function(){
-    $('.team_table').addClass("collapse");
-    $('#deff_t').removeClass("collapse");
-    $('.t_sts_btn').removeClass("sts_active");
-    $('#t_deff').addClass("sts_active");
+    addAndRemoveClass('.team_table', '#deff_t', '.t_sts_btn', '#t_deff')
   });
   $("#t_book").click(function(){
-    $('.team_table').addClass("collapse");
-    $('#book_t').removeClass("collapse");
-    $('.t_sts_btn').removeClass("sts_active");
-    $('#t_book').addClass("sts_active");
+    addAndRemoveClass('.team_table', '#book_t', '.t_sts_btn', '#t_book')
   });
   $("#p_over").click(function(){
-    $('.pl_table').addClass("collapse");
-    $('#over_p').removeClass("collapse");
-    $('.p_sts_btn').removeClass("sts_active");
-    $('#p_over').addClass("sts_active");
+    addAndRemoveClass('.pl_table', '#over_p', '.p_sts_btn', '#p_over')
   });
   $("#p_offe").click(function(){
-    $('.pl_table').addClass("collapse");
-    $('#offe_p').removeClass("collapse");
-    $('.p_sts_btn').removeClass("sts_active");
-    $('#p_offe').addClass("sts_active");
+    addAndRemoveClass('.pl_table', '#offe_p', '.p_sts_btn', '#p_offe')
   });
   $("#p_deff").click(function(){
-    $('.pl_table').addClass("collapse");
-    $('#deff_p').removeClass("collapse");
-    $('.p_sts_btn').removeClass("sts_active");
-    $('#p_deff').addClass("sts_active");
+    addAndRemoveClass('.pl_table', '#deff_p', '.p_sts_btn', '#p_deff')
   });
   $("#p_book").click(function(){
-    $('.pl_table').addClass("collapse");
-    $('#book_p').removeClass("collapse");
-    $('.p_sts_btn').removeClass("sts_active");
-    $('#p_book').addClass("sts_active");
+    addAndRemoveClass('.pl_table', '#book_p', '.p_sts_btn', '#p_book')
   });
 });
-
-// TEAMS SHOW VIEW
 
 $(document).ready( function(){
   $("#sp_over").click(function(){
-    $('.players_table').addClass("collapse");
-    $('#over_sp').removeClass("collapse");
-    $('.p_sts_btn').removeClass("sts_active");
-    $('#sp_over').addClass("sts_active");
+    addAndRemoveClass('.players_table', '#over_sp', '.p_sts_btn', '#sp_over')
   });
   $("#sp_offe").click(function(){
-    $('.players_table').addClass("collapse");
-    $('#offe_sp').removeClass("collapse");
-    $('.p_sts_btn').removeClass("sts_active");
-    $('#sp_offe').addClass("sts_active");
+    addAndRemoveClass('.players_table', '#offe_sp', '.p_sts_btn', '#sp_offe')
   });
   $("#sp_deff").click(function(){
-    $('.players_table').addClass("collapse");
-    $('#deff_sp').removeClass("collapse");
-    $('.p_sts_btn').removeClass("sts_active");
-    $('#sp_deff').addClass("sts_active");
+    addAndRemoveClass('.players_table', '#deff_sp', '.p_sts_btn', '#sp_deff')
   });
   $("#sp_book").click(function(){
-    $('.players_table').addClass("collapse");
-    $('#book_sp').removeClass("collapse");
-    $('.p_sts_btn').removeClass("sts_active");
-    $('#sp_book').addClass("sts_active");
+    addAndRemoveClass('.players_table', '#book_sp', '.p_sts_btn', '#sp_book')
   });
 });
 
+$(document).ready( function(){
+  $("#pl_over").click(function(){
+    addAndRemoveClass('.all_players_table', '#over_pl', '.p_sts_btn', '#pl_over')
+  });
+  $("#pl_offe").click(function(){
+    addAndRemoveClass('.all_players_table', '#offe_pl', '.p_sts_btn', '#pl_offe')
+  });
+  $("#pl_deff").click(function(){
+    addAndRemoveClass('.all_players_table', '#deff_pl', '.p_sts_btn', '#pl_deff')
+  });
+  $("#pl_book").click(function(){
+    addAndRemoveClass('.all_players_table', '#book_pl', '.p_sts_btn', '#pl_book')
+  });
+});
 
-$(document).ready( function() {
-  if ($(window).width() < 576) {
-    $('.bigger_h1').append( $('.shirt'));
-    $('.img_space').removeClass("col-sm-4");
+$(document).ready( function(){
+  $("#m_over").click(function(){
+    addAndRemoveClass('.team_matches', '#over_m', '.p_sts_btn', '#m_over')
+  });
+  $("#m_offe").click(function(){
+    addAndRemoveClass('.team_matches', '#offe_m', '.p_sts_btn', '#m_offe')
+  });
+  $("#m_deff").click(function(){
+    addAndRemoveClass('.team_matches', '#deff_m', '.p_sts_btn', '#m_deff')
+  });
+  $("#m_book").click(function(){
+    addAndRemoveClass('.team_matches', '#book_m', '.p_sts_btn', '#m_book')
+  });
+});
+
+$(window).on("load resize", function(e){
+  if (screen.width < 768) {
+    addAndRemoveCollapse('.longn', '.shortn')
   }
   else {
-    $('.img_space').append( $('.shirt'));
-    $('.img_space').addClass("col-sm-4");
-  }
+    addAndRemoveCollapse('.shortn', '.longn')
+  };
 });
 
 $(window).on("load resize", function(e) {
@@ -122,71 +109,13 @@ $(window).on("load resize", function(e) {
   }
 });
 
-// PLAYERS INDEX VIEW
+function addAndRemoveClass(addCollapse, removeCollapse, removeActive, addActive) {
+  addAndRemoveCollapse(addCollapse, removeCollapse)
+  $(removeActive).removeClass("sts_active");
+  $(addActive).addClass("sts_active");
+}
 
-$(document).ready( function(){
-  $("#pl_over").click(function(){
-    $('.all_players_table').addClass("collapse");
-    $('#over_pl').removeClass("collapse");
-    $('.p_sts_btn').removeClass("sts_active");
-    $('#pl_over').addClass("sts_active");
-  });
-  $("#pl_offe").click(function(){
-    $('.all_players_table').addClass("collapse");
-    $('#offe_pl').removeClass("collapse");
-    $('.p_sts_btn').removeClass("sts_active");
-    $('#pl_offe').addClass("sts_active");
-  });
-  $("#pl_deff").click(function(){
-    $('.all_players_table').addClass("collapse");
-    $('#deff_pl').removeClass("collapse");
-    $('.p_sts_btn').removeClass("sts_active");
-    $('#pl_deff').addClass("sts_active");
-  });
-  $("#pl_book").click(function(){
-    $('.all_players_table').addClass("collapse");
-    $('#book_pl').removeClass("collapse");
-    $('.p_sts_btn').removeClass("sts_active");
-    $('#pl_book').addClass("sts_active");
-  });
-});
-
-$(document).ready( function(){
-  if ($(window).width() < 515) {
-    $('.shortn').removeClass("collapse");
-    $('.longn').addClass("collapse")
-  }
-  else {
-    $('.shortn').addClass("collapse")
-    $('.longn').removeClass("collapse");
-  };
-});
-
-// PLAYERS SHOW VIEW
-
-$(document).ready( function(){
-  $("#m_over").click(function(){
-    $('.team_matches').addClass("collapse");
-    $('#over_m').removeClass("collapse");
-    $('.p_sts_btn').removeClass("sts_active");
-    $('#m_over').addClass("sts_active");
-  });
-  $("#m_offe").click(function(){
-    $('.team_matches').addClass("collapse");
-    $('#offe_m').removeClass("collapse");
-    $('.p_sts_btn').removeClass("sts_active");
-    $('#m_offe').addClass("sts_active");
-  });
-  $("#m_deff").click(function(){
-    $('.team_matches').addClass("collapse");
-    $('#deff_m').removeClass("collapse");
-    $('.p_sts_btn').removeClass("sts_active");
-    $('#m_deff').addClass("sts_active");
-  });
-  $("#m_book").click(function(){
-    $('.team_matches').addClass("collapse");
-    $('#book_m').removeClass("collapse");
-    $('.p_sts_btn').removeClass("sts_active");
-    $('#m_book').addClass("sts_active");
-  });
-});
+function addAndRemoveCollapse(addCollapse, removeCollapse) {
+  $(addCollapse).addClass("collapse");
+  $(removeCollapse).removeClass("collapse");
+}
